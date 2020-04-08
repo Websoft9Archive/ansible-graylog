df7bf42e888ad46dcd507cfbf50c3d38bd28dc46
Mozlk <xu_junkai@foxmail.com>
2020-04-08 11:21:47 +0800

    123

---
+++ roles/graylog2/files/config_domain.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+trap "rm -rf $0" EXIT
+read -p "Input You Domain:" domain
+rm -rf /data/wwwroot/graylog.html
+rm -rf /etc/nginx/conf.d/default.conf
+mv /etc/nginx/conf.d/default.conf.graylog /etc/nginx/conf.d/default.conf
+sed -i "s/www.example.com/$domain/" /etc/nginx/conf.d/default.conf
+systemctl restart nginx
